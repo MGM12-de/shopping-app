93c69f27305f43521fe8a9b9e8736809ccbfb5db
miggi12 <miguel.gebhardt@googlemail.com>
2021-02-17 19:03:32 +0100

    changed Model

--- lib/services/models.dart
+++ lib/services/models.dart
@@ -2,13 +2,33 @@
 class Fridge {
   String owner;
   String fridgeName;
+  bool defaultFridge;
+  List<FridgeUser> users;
 
-  Fridge({this.owner, this.fridgeName});
+  Fridge({this.owner, this.fridgeName, this.defaultFridge, this.users});
 
   factory Fridge.fromMap(Map data){
     return Fridge(
       fridgeName: data['fridgeName'] ?? '',
-      owner: data['uid']
+      owner: data['uid'], 
+      defaultFridge: data['defaultFridge'] ?? true,
+      users: data['users']
+    );
+  }
+}
+
+class FridgeUser{
+  String uid;
+  bool isDefaultFridge;
+  bool isOwner;
+
+  FridgeUser({this.uid, this.isDefaultFridge, this.isOwner});
+
+  factory FridgeUser.fromMap(Map data){
+    return FridgeUser(
+      uid: data['uid'],
+      isDefaultFridge: data['isDefaultFridge'] ?? true,
+      isOwner: data['isOwner'] ?? false
     );
   }
 }

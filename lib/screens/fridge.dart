a9bb35f413f25c92cdfd1ad94f444fdb751759f1
miggi12 <miguel.gebhardt@googlemail.com>
2021-02-17 18:14:48 +0100

    added appbar 2 fridge

--- lib/screens/fridge.dart
+++ lib/screens/fridge.dart
@@ -4,7 +4,9 @@ class FridgeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Fridge'),
+      ),
     );
   }
 }

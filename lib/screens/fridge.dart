7b422707482885c04c85c8fa0aa6dad7deb7175b
miggi12 <miguel.gebhardt@googlemail.com>
2021-02-18 03:45:43 +0100

    added storage of icon

--- lib/screens/fridge.dart
+++ lib/screens/fridge.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:einkaufsapp/services/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:einkaufsapp/shared/shared.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FridgeScreen extends StatelessWidget {
 
@@ -11,52 +13,71 @@ class FridgeScreen extends StatelessWidget {
   Widget build(BuildContext context){
     User user = Provider.of<User>(context);
     Document<Fridge> fridgeRef = Document<Fridge>(path: 'fridge', id: user.uid);
-    
+
     return FutureBuilder(
       future: fridgeRef.getData(),
       builder: (BuildContext context, AsyncSnapshot snap){
+        Fridge fridge = snap.data;
+
         if(snap.hasData){
           return Scaffold(
             appBar: AppBar(
-              title: Text('Fridge'),
+              title: Text(fridge.fridgeName),
             ),
             body: Center(
-              child: ListView(
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.emoji_food_beverage),
-                    title: Text("Tee"),
-                  ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.cheese, color: Colors.yellow,),
-                    title: Text("Käse")
-                  ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.apple, color: Colors.red,),
-                    title: Text("Apfel")
-                  ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.cookie, color: Colors.brown,),
-                    title: Text("Kekse"),
-                    trailing: Text("5x"),
-                    subtitle: Text("ttt"),
-                    isThreeLine: true,
-
-                  )
-                ],
-              )
+              child: getList(fridge.food),
             ),
             floatingActionButton: FloatingActionButton(
-                    onPressed: () async {  },
+                    onPressed: () async { 
+                      Food foodListItem = new Food(name: 'test1', 
+                                                    iconID: FontAwesomeIcons.footballBall.codePoint, 
+                                                    iconFontFamily: FontAwesomeIcons.footballBall.fontFamily, 
+                                                    iconFontPackage: FontAwesomeIcons.footballBall.fontPackage );
+                      List<Map> test = new List<Map>();
+                      test.add(Food.toMap(foodListItem));
+                      fridgeRef.upsert({'food': FieldValue.arrayUnion(test) });
+                     },
                     child: Icon(Icons.add),
                     backgroundColor: Theme.of(context).accentColor,
                     
                   ),
           );
         } else {
-return LoadingScreen();
+          return LoadingScreen();
         }
       },
+    );
+  }
+
+  Widget getList(List<Food> foods){
+    print(foods);
+    if(foods != null){
+      return ListView(
+                children: foods.map((e) => FoodItem(food: e)).toList()
+              );
+    } else {
+      return Text("no Data");
+    }
+  }
+}
+
+
+class FoodItem extends StatelessWidget{
+  final Food food;
+  const FoodItem({Key key, this.food}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    IconData icon = new IconData(food.iconID, fontFamily: food.iconFontFamily, fontPackage: food.iconFontPackage); 
+    return Container(
+      child: Card(
+        color: Theme.of(context).accentColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+        child: ListTile(
+          title: Text(food.name),
+          leading: Icon(icon),
+        ),
+      ),
     );
   }
 }
--- lib/services/models.dart
+++ lib/services/models.dart
@@ -1,24 +1,59 @@
 
+import 'package:einkaufsapp/services/services.dart';
+import 'package:flutter/material.dart';
+
 class Fridge {
   String owner;
   String fridgeName;
   bool defaultFridge;
   List<FridgeUser> users;
   String createdBy;
+  List<Food> food;
 
-  Fridge({this.owner, this.fridgeName, this.defaultFridge, this.users, this.createdBy});
+  Fridge({this.owner, this.fridgeName, this.defaultFridge, this.users, this.createdBy, this.food});
 
   factory Fridge.fromMap(Map data){
     return Fridge(
-      fridgeName: data['fridgeName'] ?? '',
+      fridgeName: data['fridgeName'] ?? 'Fridge',
       owner: data['uid'], 
       defaultFridge: data['defaultFridge'] ?? true,
       users: data['users'],
-      createdBy: data['createdBy']
+      createdBy: data['createdBy'],
+      food: (data['food'] as List ?? []).map((v) => Food.fromMap(v)).toList()
     );
   }
 }
 
+
+class Food {
+  String name;
+  int iconID;
+  String iconFontFamily;
+  String iconFontPackage;
+  String iconColor;
+
+  Food({this.name, this.iconID, this.iconFontFamily, this.iconFontPackage, this.iconColor});
+
+  factory Food.fromMap(Map data){
+    return Food(
+      name: data['name'] ?? 'Food',
+      iconID: data['iconID'],
+      iconFontFamily: data['iconFontFamily'],
+      iconFontPackage: data['iconFontPackage'],
+      iconColor: data['iconColor'] ?? 'red'
+    );
+  }
+
+  static Map toMap(Food data){
+    return {
+            'name': data.name,
+            'iconColor': data.iconColor,
+            'iconID': data.iconID,
+            'iconFontPackage': data.iconFontPackage,
+            'iconFontFamily': data.iconFontFamily
+          };
+  }
+}
 class FridgeUser{
   String uid;
   bool isDefaultFridge;

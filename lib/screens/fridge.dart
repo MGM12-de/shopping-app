c45084c9c103dc50c2a6888eaaf52b268e9e1416
miggi12 <miguel.gebhardt@googlemail.com>
2021-02-20 04:23:00 +0100

    added icon picker added form 2 add food

--- lib/screens/fridge.dart
+++ lib/screens/fridge.dart
@@ -1,18 +1,97 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:einkaufsapp/services/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_iconpicker/flutter_iconpicker.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:einkaufsapp/shared/shared.dart';
 import 'package:flutter/material.dart';
 
 class FridgeScreen extends StatelessWidget {
+  Document<Fridge> fridgeRef;
+  final foodInputController = TextEditingController();
+  IconData _icon;
+  Food foodListItem;
+  bool isAdaptive = true;
+  bool showTooltips = false;
+  bool showSearch = true;
+
+  createDialog(BuildContext context){
+    return showDialog(context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("Test Dialog"),
+
+        content: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                autofocus: true,
+                controller: foodInputController,
+                decoration: InputDecoration(
+                  labelText: "Food",
+                  hintText: "Bierkasten"
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Icon(_icon),
+                    RaisedButton(
+                      onPressed: () => {
+                        _pickIcon(context)
+                      },
+                      child: Text("Select an Icon"),
+                    )
+                  ],
+                ),
+              ),
+              
+            ],
+          ),
+        ),
+        actions: [
+          FlatButton(
+            child: Text("Save"),
+            onPressed: () {
+              foodListItem = new Food(name: foodInputController.text, 
+                              iconID: _icon.codePoint, 
+                              iconFontFamily: _icon.fontFamily, 
+                              iconFontPackage: _icon.fontPackage );
+              List<Map> test = new List<Map>();
+              test.add(Food.toMap(foodListItem));
+              fridgeRef.upsert({'food': FieldValue.arrayUnion(test) });
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      );
+    });
+  }
+
+  _pickIcon(BuildContext context) async {
+    IconData icon = await FlutterIconPicker.showIconPicker(
+      context,
+      iconSize: 40,
+      iconPickerShape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      title: Text('Pick an icon',
+          style: TextStyle(fontWeight: FontWeight.bold)),
+      closeChild: Text(
+          'Close',
+          textScaleFactor: 1.25,
+      ),
+      searchHintText: 'Search icon...',
+      noResultsText: 'No results for:'
+    );
+    _icon = icon;
+    (context as Element).reassemble(); // refresh screen
+  }
 
   @override
   Widget build(BuildContext context){
     User user = Provider.of<User>(context);
-    Document<Fridge> fridgeRef = Document<Fridge>(path: 'fridge', id: user.uid);
+    fridgeRef = Document<Fridge>(path: 'fridge', id: user.uid);
 
     return FutureBuilder(
       future: fridgeRef.getData(),
@@ -23,19 +102,21 @@ class FridgeScreen extends StatelessWidget {
           return Scaffold(
             appBar: AppBar(
               title: Text(fridge.fridgeName),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () {
+                    fridgeRef.getData();
+                  },
+                ),
+              ],
             ),
             body: Center(
               child: getList(fridge.food),
             ),
             floatingActionButton: FloatingActionButton(
-                    onPressed: () async { 
-                      Food foodListItem = new Food(name: 'test1', 
-                                                    iconID: FontAwesomeIcons.footballBall.codePoint, 
-                                                    iconFontFamily: FontAwesomeIcons.footballBall.fontFamily, 
-                                                    iconFontPackage: FontAwesomeIcons.footballBall.fontPackage );
-                      List<Map> test = new List<Map>();
-                      test.add(Food.toMap(foodListItem));
-                      fridgeRef.upsert({'food': FieldValue.arrayUnion(test) });
+                    onPressed: () { 
+                      createDialog(context);
                      },
                     child: Icon(Icons.add),
                     backgroundColor: Theme.of(context).accentColor,
@@ -60,8 +141,6 @@ class FridgeScreen extends StatelessWidget {
     }
   }
 }
-
-
 class FoodItem extends StatelessWidget{
   final Food food;
   const FoodItem({Key key, this.food}) : super(key: key);

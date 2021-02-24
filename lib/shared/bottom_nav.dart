b18eb89303a64ec1cc0259a4705f91c06506d56b
miggi12 <miguel.gebhardt@googlemail.com>
2021-02-24 14:27:20 +0100

    Added scan nav icon

--- lib/shared/bottom_nav.dart
+++ lib/shared/bottom_nav.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class AppBottomNav extends StatelessWidget { 
+class AppBottomNav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home, color: Theme.of(context).accentColor), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.shopping_cart, color: Theme.of(context).accentColor), label: 'Shopping List'),
-            BottomNavigationBarItem(icon: Icon(Icons.food_bank, color: Theme.of(context).accentColor,), label: 'Fridge'),
-            BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle, color: Theme.of(context).accentColor), label: 'Profile')
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Theme.of(context).accentColor),
+            label: 'Home'),
+        BottomNavigationBarItem(
+            icon:
+                Icon(Icons.shopping_cart, color: Theme.of(context).accentColor),
+            label: 'Shopping List'),
+        BottomNavigationBarItem(
+            icon: Icon(
+              FontAwesomeIcons.barcode,
+              color: Theme.of(context).accentColor,
+            ),
+            label: 'Scan'),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.food_bank,
+              color: Theme.of(context).accentColor,
+            ),
+            label: 'Fridge'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle,
+                color: Theme.of(context).accentColor),
+            label: 'Profile')
       ].toList(),
       fixedColor: Theme.of(context).accentColor,
       onTap: (int idx) {
@@ -20,9 +40,12 @@ class AppBottomNav extends StatelessWidget {
             // Navigator.pushNamed(context, '/about');
             break;
           case 2:
+            // Navigator.pushNamed(context, '/scanProdukt');
+            break;
+          case 3:
             Navigator.pushNamed(context, '/fridge');
             break;
-          case 3: 
+          case 4:
             Navigator.pushNamed(context, '/profile');
             break;
         }
